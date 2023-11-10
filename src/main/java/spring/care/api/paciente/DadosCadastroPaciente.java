fb738087dbb3e21624b3e374ec7702d2e6c35c1d
gabfiterman <gfiterman96@gmail.com>
2023-11-10 10:46:39 -0300

    FEAT: cadastro Paciente

---
+++ src/main/java/spring/care/api/paciente/DadosCadastroPaciente.java
@@ -0,0 +1,6 @@
+package spring.care.api.paciente;
+
+import spring.care.api.endereco.DadosEndereco;
+
+public record DadosCadastroPaciente(String nome, String email, String telefone, String cpf, DadosEndereco endereco) {
+}
