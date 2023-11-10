b51997fda743c9720e2979ba891719aa1d23597e
gabfiterman <gfiterman96@gmail.com>
2023-11-10 17:13:03 -0300

    PATCH: created JPA mapping for medico and endereco

---
+++ src/main/java/spring/care/api/medico/Medico.java
@@ -0,0 +1,14 @@
+package spring.care.api.medico;
+
+import spring.care.api.endereco.Endereco;
+
+public class Medico {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    private Especialidade especialidade;
+    private Endereco endereco;
+
+}
