1e2a156348154d82f294d61c92d9ecdc1d561301
gabfiterman <gfiterman96@gmail.com>
2023-11-10 16:08:50 -0300

    CHORE: add Postgres DB connection

--- src/main/java/spring/care/api/ApiApplication.java
+++ src/main/java/spring/care/api/ApiApplication.java
@@ -2,12 +2,19 @@ package spring.care.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		String databaseUrl = dotenv.get("DATABASE_URL");
+		String databaseUsername = dotenv.get("POSTGRES_USERNAME");
+		String databasePassword = dotenv.get("POSTGRES_PASSWORD");
+		String databasePort = dotenv.get("DATABASE_PORT");
+
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
 }
