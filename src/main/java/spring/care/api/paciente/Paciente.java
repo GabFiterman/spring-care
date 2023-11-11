689d5fe33450d8b59dec70ceef81f66b75471a36
gabfiterman <gfiterman96@gmail.com>
2023-11-11 19:28:03 -0300

    CHORE: create paciente Entity

---
+++ src/main/java/spring/care/api/paciente/Paciente.java
@@ -0,0 +1,22 @@
+package spring.care.api.paciente;
+
+import jakarta.persistence.*;
+import spring.care.api.endereco.Endereco;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+
+    @Embedded
+    private Endereco endereco;
+
+}
