df509499502f80e71cd5c81ba81afecc73a70172
gabfiterman <gfiterman96@gmail.com>
2023-11-11 17:42:26 -0300

    FEAT: created GET medicos

--- src/main/java/spring/care/api/controller/MedicoController.java
+++ src/main/java/spring/care/api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package spring.care.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import spring.care.api.medico.DadosCadastroMedico;
+import spring.care.api.medico.DadosListagemMedico;
 import spring.care.api.medico.Medico;
 import spring.care.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
-    
+
     @Autowired
     private MedicoRepository repository;
 
@@ -24,5 +28,9 @@ public class MedicoController {
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
-}
 
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    }
+}
