f0bb9b0a492de4f4d6479144e936d9f623886e1c
GabFiterman <gfiterman96@gmail.com>
2023-11-11 22:03:42 -0300

    FEAT: allow GET pacientes pageble

--- src/main/java/spring/care/api/controller/PacienteController.java
+++ src/main/java/spring/care/api/controller/PacienteController.java
@@ -3,6 +3,9 @@ package spring.care.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,9 +33,7 @@ public class PacienteController {
     }
 
     @GetMapping
-    public List<DadosListagemPaciente> listar() {
-        return repository.findAll().stream()
-                .map(DadosListagemPaciente::new)
-                .toList();
+    public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 5, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
