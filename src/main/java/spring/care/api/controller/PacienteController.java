177ff80cf69883a7a5b96cd61f16e204c2784057
GabFiterman <gfiterman96@gmail.com>
2023-11-11 21:25:23 -0300

    FEAT: created GET pacientes

--- src/main/java/spring/care/api/controller/PacienteController.java
+++ src/main/java/spring/care/api/controller/PacienteController.java
@@ -1,6 +1,9 @@
 package spring.care.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import spring.care.api.paciente.DadosCadastroPaciente;
+import spring.care.api.paciente.DadosListagemPaciente;
 import spring.care.api.paciente.Paciente;
 import spring.care.api.paciente.PacienteRepository;
 
@@ -23,5 +27,12 @@ public class PacienteController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
         repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemPaciente> listar() {
+        return repository.findAll().stream()
+                .map(DadosListagemPaciente::new)
+                .toList();
     }
 }
