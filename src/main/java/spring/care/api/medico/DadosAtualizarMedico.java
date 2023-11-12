6ae050f854b7512881144bdaca0df24356c4969f
GabFiterman <gfiterman96@gmail.com>
2023-11-12 02:20:08 -0300

    MINOR: formating

--- src/main/java/spring/care/api/medico/DadosAtualizarMedico.java
+++ src/main/java/spring/care/api/medico/DadosAtualizarMedico.java
@@ -3,14 +3,13 @@ package spring.care.api.medico;
 import jakarta.validation.constraints.NotNull;
 import spring.care.api.endereco.DadosEndereco;
 
-public record DadosAtualizarMedico (
-    
-    @NotNull
-    Long id, 
-    String nome, 
-    String email,
-    String telefone,
-    Especialidade especialidade,
-    DadosEndereco endereco) {
-    
+public record DadosAtualizarMedico(
+
+        @NotNull Long id,
+        String nome,
+        String email,
+        String telefone,
+        Especialidade especialidade,
+        DadosEndereco endereco) {
+
 }
